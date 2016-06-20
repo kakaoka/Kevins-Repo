b0dca32f8284e142c2f51197815afef7cdddfddb
kakaoka <kakaoka@ecofishresearch.com>
2016-06-19 20:17:27 -0700

    Jun 19

--- R_Scripts/baci-power.r
+++ R_Scripts/baci-power.r
@@ -3,6 +3,8 @@
 # 2013-02-04 CJS Added code to compute the one-sided power for the specified BACI contrast as well
 # 2013-12-16 CJS Modified code to deal with changes from new release of lmer()
 # New stuff!
+
+# NEW STUFF HI KEVIN
 # This function computes the power for a BACI design with multiple treatment
 # and control sites, multiple years measured before and after treatment is applied, and
 # sub-sampling at each year-site combination.
